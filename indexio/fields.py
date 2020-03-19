62c782da3de8b7038ae104fe62508ec4c3a70542
Sergey <xxsmotur@gmail.com>
2020-03-19 23:46:50 +0500

    Handling of the schema type

--- indexio/fields.py
+++ indexio/fields.py
@@ -7,7 +7,11 @@ class Field:
 
 
 class Text(Field):
-    def __init__(self, name, value, *args, **kwargs):
+    def __init__(self, name:str, value:str, *args, **kwargs):
         self._name = name
         self._value
         super(Text, self).__init__()
+
+class Number(Field):
+    def __init__(self, name:str, value, *args, **kwargs):
+        super(Number, self).__init__()
--- indexio/schema.py
+++ indexio/schema.py
@@ -1,7 +1,13 @@
 import json
 from typing import Dict, Any
-from fields import Text
-from consts import TEXT_TYPE, NUM_TYPE
+from fields import Field, Text, Number
+from consts import TEXT_TYPE, NUMBER_TYPE
+
+class UnknownTypeException(Exception):
+    pass
+
+class NotFoundTypeException(Exception):
+    pass
 
 class Schema:
     '''
@@ -36,5 +42,25 @@ class SchemaJSON(Schema):
     def _create_fields(self, raw_data:Dict[str,Any]):
         fields = []
         for key, value in raw_data.items():
-            if type(value) is str:
-                print('True')
+            self._type_handling(value)
+    
+    def _type_handling(self, value:Any):
+        if type(value) is str:
+           field_init = self._get_type_of_field(value)
+        if type(value) is dict:
+            if 'type' not in value:
+                raise NotFoundTypeException(f'type is not defined at {value}')
+            field_init = self._get_type_of_field(value['type'])
+        print(field_init)
+            
+
+    def _get_type_of_field(self, value:Any) -> Field:
+        '''
+        return specified type of the field
+        '''
+        if value == TEXT_TYPE:
+            return Text
+        elif value == NUMBER_TYPE:
+            return Number
+        else:
+            raise UnknownTypeException(f'{value} is unknown')
